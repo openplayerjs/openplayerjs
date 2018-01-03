3994edff3fc801421d61002659114d1a09225356
Rafael Miranda <rafa8626@gmail.com>
2018-01-03 00:55:29 -0500

    Changed variable name

--- openmedia.js
+++ openmedia.js
@@ -1,7 +1,7 @@
 import Player from './src/player';
 
 const targets = document.querySelectorAll('video.om__player, audio.om__player, iframe.om__player');
-const cached = [];
+const instances = [];
 for (let i = 0, total = targets.length; i < total; i++) {
-    cached.push(new Player(targets[i]));
+    instances.push(new Player(targets[i]));
 }
