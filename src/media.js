7d9335cd6da63f970cf1f02690d8a118ad0eeba6
Rafael Miranda <rafa8626@gmail.com>
2018-01-08 16:36:18 -0500

    Added more HTML5 wrapper methods to mimic HTML5 media functions

--- src/media.js
+++ src/media.js
@@ -21,18 +21,24 @@ class Media {
         return this;
     }
 
-    setSrc(url) {
-        this.element.src = url;
+    canPlayType(mimeType) {
+        return this.media.canPlayType(mimeType);
     }
 
     /**
-     * Check URL and process it according to its type
+     * Check URL and process it according to its type.
+     *
+     * It requires to run with Promises to avoid racing errors between execution of the action
+     * and the time the potential libraries are loaded completely.
      *
      */
     load() {
         if (!this.url) {
             throw new TypeError('URL not set');
         }
+
+        // @todo: Add method to grab correct MIME type
+        const mimeType = 'video/mp4';
 
         try {
             if (/.m3u8/.test(this.url)) {
@@ -48,7 +54,44 @@ class Media {
             }
         }
 
-        this.media.load();
+        try {
+            if (!this.canPlayType(mimeType)) {
+                throw new TypeError('Media cannot play the desired media');
+            }
+            this.media.promise.then(() => {
+                this.media.load();
+            });
+        } catch (e) {
+            // destroy media
+            this.media.destroy();
+        }
+    }
+
+    play() {
+        this.promisePlay = new Promise(resolve => {
+            resolve();
+        }).then(() => {
+            // Wait until any other Promise is resolved to execute the Play action
+            this.media.promise.then(() => {
+                this.media.play();
+            });
+        });
+
+        return this.promisePlay;
+    }
+
+    pause() {
+        this.promisePlay.then(() => {
+            this.media.pause();
+        });
+    }
+
+    set src(url) {
+        this.element.src = url;
+    }
+
+    get src() {
+        return this.element.src;
     }
 }
 
