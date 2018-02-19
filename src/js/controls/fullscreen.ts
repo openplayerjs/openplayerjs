9989949233e51c31ed84f16624fed8842050da50
Rafael Miranda <rafael.miranda@therasim.com>
2018-02-19 18:13:21 -0500

    Added code for Fullscreen button and added missing workflow to make Ads fullscreen; added missing config on Webpack to override zIndex value

--- src/js/controls/fullscreen.ts
+++ src/js/controls/fullscreen.ts
@@ -1,15 +1,17 @@
 import IEvent from '../components/interfaces/general/event';
+import { addEvent } from '../events';
+import Ads from '../media/ads';
 import Player from '../player';
-import { isVideo } from '../utils/dom';
 
 class Fullscreen {
     public player: Player;
+    public fullScreenEnabled: boolean;
     private button: HTMLButtonElement;
     private events: IEvent;
 
     /**
      *
-     * @param {Player} media
+     * @param {Player} player
      * @returns {Fullscreen}
      * @memberof Fullscreen
      */
@@ -20,14 +22,82 @@ class Fullscreen {
         this.button.className = 'om-controls__fullscreen';
         this.button.innerHTML = '<span class="om-sr">Fullscreen</span>';
 
+        // Initialize default value for fullscreen
+        const wrapper = (this.player.element.parentNode as any);
+        wrapper.setAttribute('data-fullscreen', 'false');
+
+        // Check if fullscreen is supported
+        const target = (document as any);
+        this.fullScreenEnabled = !!(target.fullscreenEnabled || target.mozFullScreenEnabled ||
+            target.msFullscreenEnabled || target.webkitSupportsFullscreen ||
+            target.webkitFullscreenEnabled || document.createElement('video').webkitRequestFullScreen);
+
+        const isFullScreen = () => !!(target.fullScreen || target.webkitIsFullScreen ||
+            target.mozFullScreen || target.msFullscreenElement || target.fullscreenElement);
+
+        const handleFullscreen = () => {
+            if (isFullScreen()) {
+                if (document.exitFullscreen) {
+                    document.exitFullscreen();
+                } else if (target.mozCancelFullScreen) {
+                    target.mozCancelFullScreen();
+                } else if (document.webkitCancelFullScreen) {
+                    document.webkitCancelFullScreen();
+                } else if (target.msExitFullscreen) {
+                    target.msExitFullscreen();
+                }
+                setFullscreenData(false);
+            } else {
+                const video = (this.player.element as any);
+                if (video.requestFullscreen) {
+                    video.requestFullscreen();
+                } else if (video.mozRequestFullScreen) {
+                    video.mozRequestFullScreen();
+                } else if (video.webkitRequestFullScreen) {
+                    video.webkitRequestFullScreen();
+                } else if (video.msRequestFullscreen) {
+                    video.msRequestFullscreen();
+                }
+                setFullscreenData(true);
+            }
+            const el = this.player.activeElement();
+            if (el instanceof Ads) {
+                const e = addEvent('resize');
+                this.player.element.dispatchEvent(e);
+            }
+        };
+
+        const setFullscreenData = (state: boolean) => {
+            wrapper.setAttribute('data-fullscreen', (!!state).toString());
+            if (state) {
+                this.button.classList.add('om-controls__fullscreen--out');
+            } else {
+                this.button.classList.remove('om-controls__fullscreen--out');
+            }
+        };
+
         this.events = {};
         this.events['click'] = () => {
-            if (this.button.classList.contains('om-controls__fullscreen--out')) {
+            handleFullscreen();
+            if (isFullScreen()) {
                 this.button.classList.remove('om-controls__fullscreen--out');
             } else {
                 this.button.classList.add('om-controls__fullscreen--out');
             }
         };
+
+        document.addEventListener('fullscreenchange', () => {
+            setFullscreenData(!!(target.fullScreen || target.fullscreenElement));
+        });
+        document.addEventListener('webkitfullscreenchange', () => {
+            setFullscreenData(!!target.webkitIsFullScreen);
+        });
+        document.addEventListener('mozfullscreenchange', () => {
+            setFullscreenData(!!target.mozFullScreen);
+        });
+        document.addEventListener('msfullscreenchange', () => {
+            setFullscreenData(!!target.msFullscreenElement);
+        });
 
         return this;
     }
@@ -52,9 +122,7 @@ class Fullscreen {
      * @memberof Fullscreen
      */
     public build(container: HTMLDivElement) {
-        if (isVideo(this.player.element)) {
-            container.appendChild(this.button);
-        }
+        container.appendChild(this.button);
         return this;
     }
 }
