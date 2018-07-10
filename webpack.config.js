93acdaa777ad89a6a552c85d646f98244c34bd9d
Rafael Miranda <rafa8626@gmail.com>
2018-07-10 01:39:51 -0400

    fix(package): Downgrade cssnano version

--- webpack.config.js
+++ webpack.config.js
@@ -115,7 +115,7 @@ module.exports = {
             },
             cssProcessor: require('cssnano')({
                 zindex: false
-            }),
-        }),
+            })
+        })
     ]
 };
