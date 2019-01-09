0c02477443d3c56f5c17f9539b079b9164866daa
Rafael Miranda <rmiranda@medscape.net>
2019-01-09 07:11:13 -0500

    fix(controls): Ensured that fullscreen is the last icon on the control bar; added missing documentation

--- src/js/interfaces/control-item.ts
+++ src/js/interfaces/control-item.ts
@@ -6,9 +6,34 @@
  * @export
  */
 export default interface ControlItem {
+    /**
+     * The icon image to present the custom control
+     *
+     * @type string
+     */
     icon: string;
+    /**
+     * The custom control label (for accessibility purposes)
+     *
+     * @type string
+     */
     title: string;
+    /**
+     * The custom control position (left. middle or right)
+     *
+     * @type string
+     */
     position: string;
+    /**
+     * Flag to indicate is a custom control
+     *
+     * @type boolean
+     */
     custom?: boolean;
+    /**
+     * The custom control callback to be executed when clicking on it
+     *
+     * @memberof ControlItem
+     */
     click(): void;
 }
