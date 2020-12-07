14918008c263a9b48dd4f9b516cb2230bdea4669
Rafael Miranda <rafa8626@gmail.com>
2020-12-07 09:24:16 -0500

    Fixed package lock and rebundled files

--- dist/openplayer.js
+++ dist/openplayer.js
@@ -2321,7 +2321,7 @@ var store = __webpack_require__(38);
 (module.exports = function (key, value) {
   return store[key] || (store[key] = value !== undefined ? value : {});
 })('versions', []).push({
-  version: '3.8.0',
+  version: '3.8.1',
   mode: IS_PURE ? 'pure' : 'global',
   copyright: '© 2020 Denis Pushkarev (zloirock.ru)'
 });
