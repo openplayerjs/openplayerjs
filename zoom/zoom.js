d8a8ba1a33cfa5f94c2d421cf6a0539187a1477d
Rafael Miranda <rafa8626@gmail.com>
2021-12-04 13:21:10 -0500

    Added main zoom in/out buttons with callbacks

--- zoom/zoom.js
+++ zoom/zoom.js
@@ -73,6 +73,7 @@ function setupZoom(player) {
                 .querySelector('.zoom-container');
             if (container && container.getAttribute('aria-hidden') === 'true') {
                 container.setAttribute('aria-hidden', 'false');
+                updateThumbs();
             }
         }
     }
@@ -94,8 +95,8 @@ function setupZoom(player) {
             indicator.style['transform-origin'] = `${indicatorPos.x} ${indicatorPos.y}`;
 
             const container = player
-            .getContainer()
-            .querySelector('.zoom-container');
+                .getContainer()
+                .querySelector('.zoom-container');
             if (container && container.getAttribute('aria-hidden') === 'false' && zoom === 1) {
                 container.setAttribute('aria-hidden', 'true');
             }
@@ -211,7 +212,7 @@ function setupZoom(player) {
                 click: () => dispatchZoomChange('reset'),
             },
         ],
-        init: (p) => {
+        init: p => {
             const container = document.createElement('div');
             container.className = 'zoom-container';
             container.setAttribute('aria-hidden', 'true');
@@ -225,6 +226,21 @@ function setupZoom(player) {
             layer.className = 'zoom-layer';
             layer.setAttribute('aria-hidden', 'true');
             p.getContainer().appendChild(layer);
+
+            const zoomInBtn = document.createElement('button');
+            zoomInBtn.innerHTML = '<svg width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path><path fill="currentColor" d="M12 10h-2v2H9v-2H7V9h2V7h1v2h2v1z"></path></svg>';
+            zoomInBtn.className = 'op-zoom op-zoom__in';
+            zoomInBtn.setAttribute('aria-label', 'Increase Zoom');
+            zoomInBtn.addEventListener('click', () => dispatchZoomChange('increase'), { passive: false });
+
+            const zoomOutBtn = document.createElement('button');
+            zoomOutBtn.innerHTML = '<svg width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zM7 9h5v1H7z"></path></svg>';
+            zoomOutBtn.className = 'op-zoom op-zoom__out';
+            zoomOutBtn.setAttribute('aria-label', 'Decrease Zoom');
+            zoomOutBtn.addEventListener('click', () => dispatchZoomChange('decrease'), { passive: false });
+
+            p.getContainer().appendChild(zoomOutBtn);
+            p.getContainer().appendChild(zoomInBtn);
 
             const video = p.getElement();
             video.addEventListener('play', playEvent, { passive: false });
@@ -249,7 +265,7 @@ function setupZoom(player) {
                 button.classList.add('selected');
             }
         },
-        destroy: (p) => {
+        destroy: p => {
             const button = p.getControls().getContainer().querySelector('#zoom');
 
             clearTimeout(timerId);
@@ -266,6 +282,14 @@ function setupZoom(player) {
             layer.removeEventListener('mouseleave', handleScreenReleased);
             window.removeEventListener('resize', handleResize);
             button.remove();
+
+            const zoomInBtn = player.getContainer().querySelector('.op-zoom__in');
+            zoomInBtn.removeEventListener('click', () => dispatchZoomChange('increase'));
+            zoomInBtn.remove();
+
+            const zoomOutBtn = player.getContainer().querySelector('.op-zoom__out');
+            zoomOutBtn.removeEventListener('click', () => dispatchZoomChange('decrease'));
+            zoomOutBtn.remove();
         },
     });
 }
