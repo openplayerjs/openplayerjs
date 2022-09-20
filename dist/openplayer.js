673fb818d95eccf27273cf9d1d1f12e4e8e15a75
Rafael Miranda <rafa8626@gmail.com>
2022-09-19 22:33:09 -0400

    test config change

--- dist/openplayer.js
+++ dist/openplayer.js
@@ -4,9 +4,9 @@
 	else if(typeof define === 'function' && define.amd)
 		define([], factory);
 	else if(typeof exports === 'object')
-		exports["OpenPlayer"] = factory();
+		exports["openplayer"] = factory();
 	else
-		root["OpenPlayer"] = factory();
+		root["openplayer"] = factory();
 })(this, () => {
 return /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
