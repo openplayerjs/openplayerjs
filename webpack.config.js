04258926d6659b55fc049fa3ebcbef30420ec431
Rafael Miranda <rafa8626@gmail.com>
2022-09-20 22:12:44 -0400

    chore: Reintegrated public path in webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
         filename: '[name].min.js',
         library: {
             name: 'openplayerjs',
