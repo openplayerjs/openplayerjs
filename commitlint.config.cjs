13ecc8614263eb4e706756cfb859db1d088f7c75
Rafael Miranda <r66503596@gmail.com>
2026-03-06 09:27:57 -0500

    chore: Added new GitHub Actions to enforce Contributing Guidelines

---
+++ commitlint.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('@commitlint/types').UserConfig} */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'perf', 'refactor', 'chore', 'docs', 'test', 'build', 'ci', 'revert'],
+    ],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
+    'header-max-length': [2, 'always', 100],
+  },
+};
